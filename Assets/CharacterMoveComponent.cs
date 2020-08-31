03dc8e46068af455c2388072f44c29be36d43f79
Gabriel Cabrera <gabycabper237@gmail.com>
2020-08-31 20:01:51 +0200

    Cambios del dia 31

---
+++ Assets/CharacterMoveComponent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveComponent : MonoBehaviour
+{
+    public int speed = 10;
+    bool colision = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!colision)
+            transform.Translate((Vector3.forward * Input.GetAxis("Vertical")) * speed * Time.deltaTime);
+
+
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("enviroments"))
+        {
+            colision = true;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("enviroments"))
+        {
+            colision = false;
+        }
+    }
+}
--- Assets/RobotSphere/Assets/Scripts/RobotFreeAnim.cs
+++ Assets/RobotSphere/Assets/Scripts/RobotFreeAnim.cs
@@ -25,23 +25,23 @@ public class RobotFreeAnim : MonoBehaviour {
 	void CheckKey()
 	{
 		// Walk
-		if (Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.DownArrow))
 		{
 			anim.SetBool("Walk_Anim", true);
 		}
-		else if (Input.GetKeyUp(KeyCode.W))
+		else if (Input.GetKeyUp(KeyCode.UpArrow)|| Input.GetKeyUp(KeyCode.DownArrow))
 		{
 			anim.SetBool("Walk_Anim", false);
 		}
 
 		// Rotate Left
-		if (Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			rot[1] -= rotSpeed * Time.fixedDeltaTime;
 		}
 
 		// Rotate Right
-		if (Input.GetKey(KeyCode.D))
+		if (Input.GetKey(KeyCode.RightArrow))
 		{
 			rot[1] += rotSpeed * Time.fixedDeltaTime;
 		}
